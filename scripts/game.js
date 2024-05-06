142eb98673f06a5c2936e2352d161758a1278f14
niekados <tiny.post@yahoo.com>
2024-05-06 18:20:16 +0000

    Add circle on to game,js and tests for it

--- scripts/game.js
+++ scripts/game.js
@@ -3,19 +3,45 @@ let game = {
     currentGame: [],
     playerMoves: [],
     choices: ["button1", "button2", "button3", "button4"],
-};
+    turnNumber: 0,
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns();
+}
 
 function newGame() {
     game.score = 0;
     game.playerMoves = [];
     game.currentGame = [];
     showScore();
-};
+    addTurn();
+}
 
 function showScore () {
     document.getElementById("score").innerText = game.score;
 }
 
+function lightsOn(circle) {
+    document.getElementById(circle).classList.add("light");
+    setTimeout(() => {
+        document.getElementById(circle).classList.remove("light");
+    }, 400);
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
 // we use currly braces, as we will be exporting more than one object from this file
-module.exports = { game, newGame, showScore };
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
 
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
 // This will be always the same for any HTML you want to load in to the DOM (except changing the file name)
 beforeAll(() => {
@@ -29,12 +29,15 @@ describe("game object contains correct keys", () => {
     test("choices contains the correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+    test("turnNumber key exists in game object", () => {
+        expect("turnNumber" in game).toBe(true);
+    })
 });
 
 describe("newGame works correctly", () => {
     //We create additional beforeAll function, because we want to set up the game state 
     //with some fake values to see if the new game function resets them.
-    beforeAll(() => {   
+    beforeAll(() => {   // beforeAll runs before all of the tests
         game.score = 42; //set fake score to 42
         game.playerMoves = ["button1", "button2"];
         game.currentGame = ["button1", "button2"];
@@ -47,10 +50,37 @@ describe("newGame works correctly", () => {
     test("should clear the playerMoves array", () => {
         expect(game.playerMoves.length).toEqual(0); //can use .toBe too
     });
-    test("should clear the currentGame array", () => {
-        expect(game.currentGame.length).toEqual(0);
-    });
+    test("should be one move in the computers array", () => {
+        expect(game.currentGame.length).toBe(1);
+    })
     test("shoukd display zero for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
+    });
+});
+
+describe("gameplay works correctly", () => {
+    beforeEach(() => {      // beforeEach runs before each test is run
+        game.score = 0;
+        game.currentGame = [];
+        addTurn();
+    });
+    afterEach(() => {
+        game.score = 0;
+        game.currentGame = [];
+        game.playerMoves = [];
+    });
+    test("addTurn adds another new turn to the game", () => {
+        addTurn();
+        expect(game.currentGame.length).toBe(2);
+    });
+    test("should add correct class to light up the buttons", () => {
+        let button = document.getElementById(game.currentGame[0]);
+        lightsOn(game.currentGame[0]);
+        expect(button.classList).toContain("light");  // .toContain test if it contains
+    });
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
     });
 });
