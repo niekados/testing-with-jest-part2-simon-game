edfabc3076ed226412195d7d601cc1d4d7486357
niekados <tiny.post@yahoo.com>
2024-05-06 15:37:24 +0000

    Add game.js and game.test.js, add first test to it

---
+++ scripts/game.js
@@ -0,0 +1,10 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+};
+
+
+// we use currly braces, as we will be exporting more than one object from this file
+module.exports = { game };
