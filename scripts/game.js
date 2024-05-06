d4ad4a6a5b94e4c39b96732b4770ba026dd253eb
niekados <tiny.post@yahoo.com>
2024-05-06 16:42:14 +0000

    Add showScore function to game.js and test for it

--- scripts/game.js
+++ scripts/game.js
@@ -5,6 +5,17 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"],
 };
 
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+};
+
+function showScore () {
+    document.getElementById("score").innerText = game.score;
+}
 
 // we use currly braces, as we will be exporting more than one object from this file
-module.exports = { game };
+module.exports = { game, newGame, showScore };
+
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 // This will be always the same for any HTML you want to load in to the DOM (except changing the file name)
 beforeAll(() => {
@@ -28,5 +28,29 @@ describe("game object contains correct keys", () => {
     });
     test("choices contains the correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    //We create additional beforeAll function, because we want to set up the game state 
+    //with some fake values to see if the new game function resets them.
+    beforeAll(() => {   
+        game.score = 42; //set fake score to 42
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame(); //call new game function and it should reset the score
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the playerMoves array", () => {
+        expect(game.playerMoves.length).toEqual(0); //can use .toBe too
+    });
+    test("should clear the currentGame array", () => {
+        expect(game.currentGame.length).toEqual(0);
+    });
+    test("shoukd display zero for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
